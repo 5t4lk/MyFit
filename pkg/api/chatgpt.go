0a0c657a8cb6d4d738950b12fd79d51f8767a79a
Vladimir Shevchuk <5t4lk@Vladimirs-MacBook-Air.local>
2023-04-08 17:53:31 +0100

    test

--- pkg/api/chatgpt.go
+++ pkg/api/chatgpt.go
@@ -56,6 +56,7 @@ func unmarshalGPT(data []byte) (ChatGPT, error) {
 	return c, nil
 }
 
+// c
 type ChatGPT struct {
 	ConversationID string `json:"conversationId"`
 	Response       string `json:"response"`
